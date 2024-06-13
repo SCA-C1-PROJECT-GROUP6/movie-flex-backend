16d8693f5df2dc6d86296f1f70e86faefdc8530c
Afrakumah <opokua715@gmail.com>
2024-06-13 11:41:54 +0000

    cors configured

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 8888
 
 const app = express()
 
-app.use(cors({origin: "http://localhost:5173"}));//the react url here please
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173'
+  }));
 app.use(express.json())
 app.use(cookieParser());
 app.use(morgan('dev'))
