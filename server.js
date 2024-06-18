20be3957c9265d113c8a610c075998587d3b33f9
Afrakumah <opokua715@gmail.com>
2024-06-18 09:55:19 +0000

    allow cors to be accessed from anywhere *

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import DBconnect from './db.js'
-import morgan from 'morgan'
-import cors from 'cors'
-import cookieParser from 'cookie-parser';
-import userRoute from './routes/userRoutes.js'
-import movieRoute from './routes/movieRoute.js'
-dotenv.config()
-import { errorHandler } from './middleware/errorHandler.js'
+import express from "express";
+import dotenv from "dotenv";
+import DBconnect from "./db.js";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoutes.js";
+import movieRoute from "./routes/movieRoute.js";
+dotenv.config();
+import { errorHandler } from "./middleware/errorHandler.js";
 
-const PORT = process.env.PORT || 8888
+const PORT = process.env.PORT || 8888;
 
-const app = express()
+const app = express();
 
 // app.use(cors({
 //     credentials: true,
@@ -19,25 +19,30 @@ const app = express()
 
 //   }));
 
-  app.use(cors({credentials: true,
-    origin:['https:// 666cc6c50c30bb4afe236965--splendorous-biscuit-7dffcl.netlify.app','http://localhost:5173']}))
 
-app.use(express.json())
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "*",
+      "https://666cc6c50c30bb4afe236965--splendorous-biscuit-7dffcl.netlify.app",
+      "http://localhost:5173",
+    ],
+  })
+);
+
+app.use(express.json());
 app.use(cookieParser());
-app.use(morgan('dev'))
-app.use(express.urlencoded({extended: false}))
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/users', userRoute)
-app.use('/api/movies', movieRoute)
-app.use(errorHandler)
-
-
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-    DBconnect()
-   console.log(`server is running on PORT ${PORT}`) 
-})
-
-
+  DBconnect();
+  console.log(`server is running on PORT ${PORT}`);
+});
 
 //https://documenter.getpostman.com/view/34756068/2sA3XQggwr           api documention
